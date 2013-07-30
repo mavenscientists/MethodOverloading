551d527a4d050ab81d88ca7240d30cda46c34fdb
Karan Sharma <javakaran@live.com>
2013-07-30 10:20:00 +0530

    Method Overloading

---
+++ src/methodoverloading/MethodOverloading.java
@@ -0,0 +1,37 @@
+package methodoverloading;
+
+import java.awt.Point;
+
+/**
+ *
+ * @author  Karan Sharma
+ * @time     Jul 28, 2013 8:20:50 AM 
+ */
+public class MethodOverloading {
+
+    int a;
+    
+    public MethodOverloading(int a) {
+        this.a = a;
+        
+    }
+
+
+    
+    
+    void draw(int i, String a, Point b)
+    {
+    }
+
+        
+    int draw()
+    {
+        return 1;
+    }
+    
+    public static void main(String[] args) {
+        
+        
+    }
+
+}
